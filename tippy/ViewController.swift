9764d639f77a81827bdf9dbd4bc8434cd4fffeae
Abdur Rafey <rafey7@Abdurs-MBP.home>
2018-08-07 18:51:30 -0400

    added .gitattributes .gitignore

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var TaxLabel: UILabel!
+    @IBOutlet weak var BillField: UITextField!
+    @IBOutlet weak var TipLabel: UILabel!
+    @IBOutlet weak var TotalLabel: UILabel!
+    @IBOutlet weak var TipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func CalculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.1, 0.2, 0.3]
+        
+        let bill = Double(BillField.text!) ?? 0
+        let tax = (bill * 0.10) + bill
+        let tip = bill * tipPercentages[TipControl.selectedSegmentIndex]
+        let total = (tip + tax)
+        
+        TaxLabel.text = String(format: "$%.2f", tax)
+        TipLabel.text = String(format: "$%.2f", tip)
+        TotalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
